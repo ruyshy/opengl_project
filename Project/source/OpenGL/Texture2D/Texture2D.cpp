3bef81244116405f6802d66ad3b7472e4c182d96
ruyshy <47741373+ruyshy@users.noreply.github.com>
2024-04-10 09:58:13 +0900

    test

--- Project/source/OpenGL/Texture2D/Texture2D.cpp
+++ Project/source/OpenGL/Texture2D/Texture2D.cpp
@@ -71,8 +71,6 @@ Texture2D TextureSystem::Generate(string image)
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-		// ********* texture minimize & maxmize operation
-		// to solve pixlation problem
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //GL_LINEAR_MIPMAP_NEAREST
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	}
